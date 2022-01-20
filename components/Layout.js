5744c50f2131ed77df5903a8b76ee12784a50c30
Daniel Bergmann <dba222@outlook.com>
2022-01-20 21:34:56 +0000

    added description from data to dynamic pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
 import {
@@ -7,10 +7,40 @@ import {
   Typography,
   Container,
   Link,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
 } from '@material-ui/core';
 import useStyles from '../utils/styles';
+import { Store } from '../utils/Store';
 
 export default function Layout({ children, title, description }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+
   const classes = useStyles();
   return (
     <div>
@@ -18,28 +48,33 @@ export default function Layout({ children, title, description }) {
         <title>{title ? `${title} - Ecommerce site` : 'Ecommerce site'}</title>
         {description && <meta name='description' content={description}></meta>}
       </Head>
-      <AppBar position='static' className={classes.navbar}>
-        <Toolbar>
-          <NextLink href='/' passHref>
-            <Link>
-              <Typography className={classes.brand}>Ecommerce Site</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href='/cart' passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position='static' className={classes.navbar}>
+          <Toolbar>
+            <NextLink href='/' passHref>
+              <Link>
+                <Typography className={classes.brand}>
+                  Ecommerce Site
+                </Typography>
+              </Link>
             </NextLink>
-            <NextLink href='/login' passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href='/cart' passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href='/login' passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css?family=Roboto>300,400,500,700&display=swap'
+        />
         <body>
           <Main />
           <NextScript />
